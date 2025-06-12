94814302d91ef83b68d99fb488c552f7d1f6b654
uzair171 <jabransaleem171@gmail.com>
2025-06-12 12:23:50 +0500

    First commit with multiple tasks done 1.made buttons 2.random number genration 3.put that number to dice 4.click roll button to call random number generation again

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react"
+import Die from "./components/Die"
+
+export default function App(){
+
+  const [dieValue,setDieValue] = useState(generateAllNewDice())
+  function generateAllNewDice(){
+    // const newDice =[]
+    // for(let i=0;i<10;i++){
+    //   const randomDieNumber = Math.ceil(Math.random() * 6)
+    //   newDice.push(randomDieNumber)
+    // }
+    // return newDice
+    return new Array(10)
+                    .fill(0)
+                    .map(()=>Math.ceil(Math.random()*6))
+  }
+  const diceElements = dieValue.map((element,index)=>{
+        return <Die 
+          key = {index}
+          value = {element}
+          />
+        })
+
+  function rollElement(){
+    setDieValue(generateAllNewDice)
+  }
+
+  return(
+    <main>
+      <div className="die-div">
+        {diceElements}
+        {/* <Die value = {value[1]}/>
+        <Die value = {value[2]}/>
+        <Die value = {value[3]}/>
+        <Die value = {value[4]}/>
+        <Die value = {value[5]}/>
+        <Die value = {value[6]}/>
+        <Die value = {value[7]}/>
+        <Die value = {value[8]}/>
+        <Die value = {value[9]}/>
+        <Die value = {value[0]}/> */}
+      </div>
+
+      <button 
+      className="roll-button"
+      onClick={rollElement}
+      >
+        Roll</button>
+    </main>
+  )
+}
